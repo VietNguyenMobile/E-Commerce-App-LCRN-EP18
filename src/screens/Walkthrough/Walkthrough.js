76b9fce566e911ad455b006b6036c691cc97390a
Viet Nguyen <viet.nguyen2@hitachivantara.com>
2022-10-02 17:59:15 +0700

    first commit

---
+++ src/screens/Walkthrough/Walkthrough.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, Animated} from 'react-native';
+
+import {TextButton} from '../../components';
+import {COLORS, SIZES, constants, FONTS} from '../../constants';
+
+const Walkthrough = () => {
+
+    const scrol
+
+  return (
+    <View style={{flex: 1, backgroundColor: COLORS.light}}>
+      <Text>Walkthrough</Text>
+      <Animated.FlatList  />
+    </View>
+  );
+};
+
+export default Walkthrough;
